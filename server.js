8a02dccd393086e918029d61600970082cbe0293
erma <agpritts@gmail.com>
2022-03-18 01:41:19 -0400

    initial code/files

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const { animals } = require('./data/animals');
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        // save personalityTraits as a dedicated array
+        // if personalityTraits is a string, place it into a new array and save
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        // checks each item in the array for a specific trait, then once found puts it into the ending filteredResults array
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+app.listen(3001, () => {
+    console.log(`API server is now on port ${PORT}!`);
+});
+
+
